2d4042bb738f6f2c3933bfa57dbf308baeb18ef1
Oren Fromberg <oren@adhocteam.us>
2020-04-17 00:12:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# build
+```bash
+docker build -t axe-puppeteer-ci .
+```
+
+# run
+```bash
+docker run --init --rm --cap-add=SYS_ADMIN axe-puppeteer-ci
+```
---
+++ app/index.js
@@ -0,0 +1,21 @@
+const { AxePuppeteer } = require('axe-puppeteer')
+const puppeteer = require('puppeteer')
+
+if (process.argv.slice(2).length == 0) { 
+  console.error("please specify a website")
+} else {
+  ;(async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setBypassCSP(true)
+  
+    await page.goto(process.argv[2])
+  
+    const results = await new AxePuppeteer(page).analyze()
+    console.log(JSON.stringify(results))
+  
+    await page.close()
+    await browser.close()
+  })()
+}
+
