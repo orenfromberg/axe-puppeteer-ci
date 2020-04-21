6d5c764f7e312267dcabf4f4e819941491e0eb09
Oren Fromberg <oren@adhocteam.us>
2020-04-21 13:27:59 -0400

    Fixing output

--- README.md
+++ README.md
@@ -5,6 +5,5 @@ docker build -t axe-puppeteer-ci .
 
 # run
 ```bash
-docker run --init -it --rm --cap-add=SYS_ADMIN axe-puppeteer-ci
-pptruser@a6a6616d5f80:~/app$ node index.js https://www.google.com
+docker run --init -it --rm --cap-add=SYS_ADMIN axe-puppeteer-ci https://www.google.com
 ```
--- app/index.js
+++ app/index.js
@@ -1,5 +1,48 @@
 const { AxePuppeteer } = require('axe-puppeteer')
 const puppeteer = require('puppeteer')
+const colors = require('colors');
+const link = colors.underline.blue;
+const error = colors.red.bold;
+
+const selectorToString = (selectors, separator) => {
+	separator = separator || ' ';
+	return selectors
+		.reduce((prev, curr) => prev.concat(curr), [])
+		.join(separator);
+};
+
+const cliReporter = (...args) => {
+  console.log(...args);
+};
+
+const logResults = (results) => {
+  const { violations } = results;
+
+  if (violations.length === 0) {
+    cliReporter(colors.green('  0 violations found!'));
+    return;
+  }
+
+  const issueCount = violations.reduce((count, violation) => {
+    cliReporter(
+      '\n' +
+        error('  Violation of %j with %d occurrences!\n') +
+        '    %s. Correct invalid elements at:\n' +
+        violation.nodes
+          .map(node => '     - ' + selectorToString(node.target) + '\n')
+          .join('') +
+        '    For details, see: %s',
+      violation.id,
+      violation.nodes.length,
+      violation.description,
+      link(violation.helpUrl.split('?')[0])
+    );
+    return count + violation.nodes.length;
+  }, 0);
+
+  cliReporter(error('\n%d Accessibility issues detected.'), issueCount);
+}
+
 
 if (process.argv.slice(2).length == 0) { 
   console.error("please specify a website")
@@ -12,7 +55,7 @@ if (process.argv.slice(2).length == 0) {
     await page.goto(process.argv[2])
   
     const results = await new AxePuppeteer(page).analyze()
-    console.log(JSON.stringify(results))
+    logResults(results)
   
     await page.close()
     await browser.close()
